68bb1a84a17522d0743ce77b6da7009392d587c4
Emre KÖKÇÜ <45900778+githubemrekokcu@users.noreply.github.com>
2018-12-22 22:11:44 +0300

    Change Scanner Text

--- app/src/main/java/com/emrekokcu/developer/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/emrekokcu/developer/qrcodescanner/MainActivity.java
@@ -1,8 +1,13 @@
 package com.emrekokcu.developer.qrcodescanner;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseArray;
@@ -29,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     @SuppressLint("CutPasteId")
     private void initComponents() {
         btn_QrAndBarcode_Scanner = findViewById(R.id.btn_scanner);
@@ -39,7 +43,23 @@ public class MainActivity extends AppCompatActivity {
 
     private void registerEventHandlers() {
         btn_QrAndBarcode_Scanner_onClick();
+        txt_scannerResults_onClick();
         bindData();
+    }
+
+    private void txt_scannerResults_onClick() {
+        txt_scannerResults.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!txt_scannerResults.getText().toString().isEmpty()) {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(txt_scannerResults.getText().toString()));
+                    startActivity(i);
+                }
+
+            }
+        });
+
     }
 
     private void bindData() {
@@ -59,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
                         new BarcodeDetector.Builder(getApplicationContext())
                                 .setBarcodeFormats(Barcode.ALL_FORMATS)
                                 .build();
-                if(!detector.isOperational()){
+                if (!detector.isOperational()) {
                     txt_scannerResults.setText("Could not set up the detector!");
                     return;
                 }
@@ -70,6 +90,8 @@ public class MainActivity extends AppCompatActivity {
                 String str_result = thisCode.rawValue;
                 assert !str_result.isEmpty();
                 txt_scannerResults.setText(str_result);
+                txt_scannerResults.setTextColor(Color.BLUE);
+                txt_scannerResults.setPaintFlags(txt_scannerResults.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
             }
         });
 
